7ec16aa14e98031da37411d4e3f6d0cdb7deba92
VladymereBilan <vladymeresbilan@gmail.com>
2025-11-25 18:14:20 +0800

    Fixed the Add ExpenseButton

--- frontend/expense-tracker/src/components/Expense/ExpenseOverview.jsx
+++ frontend/expense-tracker/src/components/Expense/ExpenseOverview.jsx
@@ -4,34 +4,39 @@ import { LuPlus } from "react-icons/lu";
 import { prepareExpenseLineChartData } from "../../utils/helper";
 
 const ExpenseOverview = ({transactions, onExpenseIncome}) => {
-    const [chartData, setChartData] = useState ([]);
+        const [chartData, setChartData] = useState ([]);
 
-    useEffect(() => {
-        const result = prepareExpenseLineChartData(transactions);
-        setChartData(result);
+        useEffect(() => {
+                const result = prepareExpenseLineChartData(transactions);
+                setChartData(result);
 
-        return () => {};
-    }, [transactions]);
-  return <div className="card">
-    <div className="flex items-center justify-between">
-        <div className="">
-            <h5 className="text-lg">Expense Overview</h5>
-            <p className="text-xs text-gray-400 mt-0.5">
-                Track your spending trends over time and gain insights into where
-                your money goes.
-            </p>
+                return () => {};
+        }, [transactions]);
+
+    return (
+        <div className="card">
+            <div className="flex items-center justify-between">
+                <div>
+                    <h5 className="text-lg">Expense Overview</h5>
+                    <p className="text-xs text-gray-400 mt-0.5">
+                        Track your spending trends over time and gain insights into where
+                        your money goes.
+                    </p>
+                </div>
+
+                <button className="add-btn flex items-center gap-2" onClick={onExpenseIncome}>
+                    <LuPlus className="text-lg" />
+                    Add Expense
+                </button>
+            </div>
+
+            <div className="mt-4 text-sm text-gray-600"></div>
+
+            <div className="mt-10">
+                <CustomLineChart data={chartData} />
+            </div>
         </div>
-
-            <button className="add-btn" onClick={onExpenseIncome}>
-                <LuPlus className="text-lg" />
-                Add Expense
-            </button>
-        </div>
-
-        <div className="mt-10">
-            <CustomLineChart data={chartData} />
-        </div>
-  </div>;
+    );
   
 };
 
